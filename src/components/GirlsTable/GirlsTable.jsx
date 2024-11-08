edc1ed065e2299a000442dc9fe00683f2ea3181e
p5ycuk <p5ycuk@inf.elte.hu>
2024-11-09 00:09:35 +0100

    card cash sum

--- src/components/GirlsTable/GirlsTable.jsx
+++ src/components/GirlsTable/GirlsTable.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 import GirlsTableRow from './GirlsTableRow';
 import girlsData from './GirlsData';
 import serviceData from './ServiceData';
 
 const GirlsTable = () => {
-    return (
+    const [cash, setCash] = useState(0);
+    const [card, setCard] = useState(0);
 
+    // Összeg kiszámítása az aktuális állapotok alapján
+    const sum = cash + card;
+
+    return (
         <div>
             <div>
                 <table>
                     <thead>
                         <tr>
-                            <th>ÖSSZ BEVÉTEL TÁNC + LÁNY ITAL</th>
-                            <th>kartya + keszpenz</th>
-                            <th>keszpenz</th>
-                            <th>kartya</th>
-
+                            <th rowSpan="2">ÖSSZ BEVÉTEL TÁNC + LÁNY ITAL</th>
+                            <th rowSpan="2">{sum}</th>
+                            <th rowSpan="2">{cash}</th>
+                            <th rowSpan="2">{card}</th>
                             {serviceData.map((service, index) => (
-                                <th key={index}>
+                                <th key={`header-${index}`} colSpan="2">
                                     {service.név}
                                 </th>
                             ))}
@@ -27,19 +30,29 @@ const GirlsTable = () => {
                     <tbody>
                         <tr>
                             <td>Aktuális lányok</td>
-                            <td>Összesen</td>
-                            <td>Készpénz</td>
-                            <td>Kártya</td>
+                            <td className='girl-bg'>Összesen</td>
+                            <td className='girl-bg'>Készpénz</td>
+                            <td className='girl-bg'>Kártya</td>
+                            {serviceData.map((_, index) => (
+                                <React.Fragment key={index}>
+                                    <td>Készpénz</td>
+                                    <td>Kártya</td>
+                                </React.Fragment>
+                            ))}
                         </tr>
                         {girlsData.map((girl, index) => (
-                            <GirlsTableRow key={index} girl={girl} />
+                            <GirlsTableRow
+                                key={index}
+                                girl={girl}
+                                cash={cash} setCash={setCash}
+                                card={card} setCard={setCard}
+                            />
                         ))}
-
                     </tbody>
                 </table>
             </div>
         </div>
-    )
+    );
 }
 
-export default GirlsTable
+export default GirlsTable;
--- src/components/GirlsTable/GirlsTableRow.jsx
+++ src/components/GirlsTable/GirlsTableRow.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import serviceData from './ServiceData';
 
-const GirlsTableRow = ({ girl }) => {
-    // Ide jöhetnek a dinamikus értékek vagy statikus cellák
+const GirlsTableRow = ({ girl, cash, setCash, card, setCard }) => {
+    const [values, setValues] = useState(
+        serviceData.map(() => ({ cash: 0, card: 0 }))
+    );
+
+    const handleInputChange = (index, field, newValue) => {
+        const updatedValues = [...values];
+        updatedValues[index][field] = newValue === '' ? '' : parseInt(newValue, 10) || 0;
+        setValues(updatedValues);
+
+        // Az összeg kiszámítása és a megfelelő állapot frissítése a szülő komponensben
+        const sum = updatedValues.reduce((acc, item) => acc + (item[field] || 0), 0);
+        if (field === 'cash') {
+            setCash(sum);
+        } else if (field === 'card') {
+            setCard(sum);
+        }
+    };
+
     return (
         <tr>
             <td>{girl}</td>
-            <td>0</td> {/* Kartya + keszpenz helykitöltő érték */}
-            <td>0</td> {/* Keszpenz helykitöltő érték */}
-            <td>0</td> {/* Kartya helykitöltő érték */}
-            {/* Itt bővítheted a cellákat további adatokkal a serviceData alapján */}
-            {Array(serviceData.length).fill(0).map((value, index) => (
-                <td key={index}>{value}</td> /* Helykitöltő érték minden szolgáltatás oszlophoz */
+            <td className='girl-bg'>{cash}</td>
+            <td className='girl-bg'>{card}</td>
+            {serviceData.map((_, index) => (
+                <td key={index}>
+                    <input
+                        type="number"
+                        value={values[index].cash === 0 ? '' : values[index].cash}
+                        onChange={(e) => handleInputChange(index, 'cash', e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        value={values[index].card === 0 ? '' : values[index].card}
+                        onChange={(e) => handleInputChange(index, 'card', e.target.value)}
+                    />
+                </td>
             ))}
         </tr>
     );
