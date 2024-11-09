2bea10c00a19cbdda6af0ca2a74124e94d9ddaff
p5ycuk <p5ycuk@inf.elte.hu>
2024-11-09 15:32:06 +0100

    input onwheel

--- src/components/GirlsTable/GirlsTableRow.jsx
+++ src/components/GirlsTable/GirlsTableRow.jsx
@@ -9,17 +9,24 @@ const GirlsTableRow = ({ girl, cash, setCash, card, setCard }) => {
 
     const handleInputChange = (index, field, newValue) => {
         const updatedValues = [...values];
-        updatedValues[index][field] = newValue === '' ? '' : parseInt(newValue, 10) || 0;
+        const oldNumericValue = updatedValues[index][field];
+        const numericValue = newValue === '' ? 0 : parseInt(newValue, 10) || 0;
+
+        // Frissítjük a tárolt értékeket
+        updatedValues[index][field] = numericValue;
         setValues(updatedValues);
 
-        // Az összeg kiszámítása és a megfelelő állapot frissítése a szülő komponensben
-        const sum = updatedValues.reduce((acc, item) => acc + (item[field] || 0), 0);
+        const servicePrice = serviceData[index].ár;
+        const totalChange = numericValue * servicePrice - oldNumericValue * servicePrice;
+
+        // Frissítjük a cash vagy card állapotot
         if (field === 'cash') {
-            setCash(sum);
+            setCash((prevCash) => prevCash + totalChange);
         } else if (field === 'card') {
-            setCard(sum);
+            setCard((prevCard) => prevCard + totalChange);
         }
     };
+
 
 
     return (
@@ -35,6 +42,7 @@ const GirlsTableRow = ({ girl, cash, setCash, card, setCard }) => {
                             type="number"
                             value={values[index].cash === 0 ? '' : values[index].cash}
                             onChange={(e) => handleInputChange(index, 'cash', e.target.value)}
+                            onWheel={(e) => e.target.blur()}
                         />
                     </td>
                     <td className='number-td'>
@@ -42,6 +50,7 @@ const GirlsTableRow = ({ girl, cash, setCash, card, setCard }) => {
                             type="number"
                             value={values[index].card === 0 ? '' : values[index].card}
                             onChange={(e) => handleInputChange(index, 'card', e.target.value)}
+                            onWheel={(e) => e.target.blur()}
                         />
                     </td>
                 </React.Fragment>
