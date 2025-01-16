aec352cf9c4155ca823d1f3c2f1265df5f58fa62
p5ycuk <p5ycuk@inf.elte.hu>
2025-01-16 20:44:12 +0100

    service dropdown

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import xIcon from '../assets/x-icon.svg';
 import editIcon from '../assets/edit-icon.png';
 import '../styles/Services.css';
 
 const Services = ({ services }) => {
+    const [openTab, setOpenTab] = useState(null);
+
+    const toggleDropdown = (index) => {
+        setOpenTab(openTab === index ? null : index)
+    };
 
     return (
         <div className="services-main-content">
@@ -16,23 +21,60 @@ const Services = ({ services }) => {
                 <button className="services-btn">Hozzáadás</button>
             </div>
             {services.map((service, index) => (
-                <div
-                    className="services-row-container services-container"
-                    key={index}
-                >
-                    <span>{service.name}</span>
-                    <div className="services-actions">
-                        <img
-                            className="services-remove"
-                            src={xIcon}
-                            alt="Remove icon"
-                        />
-                        <img
-                            className="services-edit"
-                            src={editIcon}
-                            alt="Edit icon"
-                        />
+                <div>
+                    <div
+                        className="services-row-container services-container"
+                        key={index}
+                        onClick={() => toggleDropdown(index)}
+                    >
+                        <span>{service.name}</span>
+                        <div className="services-actions">
+                            <img
+                                className="services-remove"
+                                src={xIcon}
+                                alt="Remove icon"
+                            />
+                            <img
+                                className="services-edit"
+                                src={editIcon}
+                                alt="Edit icon"
+                            />
+                        </div>
                     </div>
+                    {openTab == index && (
+                        <div className="services-container services-dropdown-content">
+                            <p className='services-row-container services-container services-dropdown-item'>
+                                <div>Típus:<span>{service.type}</span></div>
+                                <div className="services-actions">
+                                    <img
+                                        className="services-edit"
+                                        src={editIcon}
+                                        alt="Edit icon"
+                                    />
+                                </div>
+                            </p>
+                            <p className='services-row-container services-container services-dropdown-item'>
+                                <div>Ár:<span>{service.price}</span></div>
+                                <div className="services-actions">
+                                    <img
+                                        className="services-edit"
+                                        src={editIcon}
+                                        alt="Edit icon"
+                                    />
+                                </div>
+                            </p>
+                            <p className='services-row-container services-container services-dropdown-item'>
+                                <div>Jutalék:<span>{service.commission}</span></div>
+                                <div className="services-actions">
+                                    <img
+                                        className="services-edit"
+                                        src={editIcon}
+                                        alt="Edit icon"
+                                    />
+                                </div>
+                            </p>
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
