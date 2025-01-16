b4e0ad5376d6febef6a719231f319c05dc44b3d7
p5ycuk <p5ycuk@inf.elte.hu>
2025-01-16 19:52:08 +0100

    service name list and style

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import '../styles/Services.css'
+import xIcon from '../assets/x-icon.svg';
+import editIcon from '../assets/edit-icon.png';
+import '../styles/Services.css';
 
 const Services = ({ services }) => {
+
     return (
-        <div className='container'>
+        <div className="services-main-content">
+            <div className="services-new-container services-container">
+                <input
+                    className="services-new-input"
+                    placeholder="Adj hozzá új szolgáltatást!"
+                    type="text"
+                />
+                <button className="services-btn">Hozzáadás</button>
+            </div>
             {services.map((service, index) => (
-                <div><p>{service.name}</p></div>
+                <div
+                    className="services-row-container services-container"
+                    key={index}
+                >
+                    <span>{service.name}</span>
+                    <div className="services-actions">
+                        <img
+                            className="services-remove"
+                            src={xIcon}
+                            alt="Remove icon"
+                        />
+                        <img
+                            className="services-edit"
+                            src={editIcon}
+                            alt="Edit icon"
+                        />
+                    </div>
+                </div>
             ))}
         </div>
     );
